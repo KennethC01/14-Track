87ba693d4664f1eef0b6211d72ebff8182da235b
KennethC01 <kennethcaballerol14@gmail.com>
2026-07-04 20:19:01 -0600

    Configurar build para Vercel

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
+
+// Asegúrate de que las rutas relativas sean exactas. 
+// Si tu archivo es 'src/app/components/login/login.component.ts', el import debe ser './components/login/login.component'
 import { LoginComponent } from './components/login/login.component';
-import { DashboardComponent } from './components/dashboard/dashboard';
-import { RegistroComponent } from './exploradores/registro/registro';
-import { AsistenciaComponent } from './exploradores/asistencia/asistencia'; 
+import { DashboardComponent } from './components/dashboard/dashboard'; // ¡Añade el .component si falta!
+import { RegistroComponent } from './exploradores/registro/registro'; // ¡Añade el .component si falta!
+import { AsistenciaComponent } from './exploradores/asistencia/asistencia';
 import { AscensoComponent } from './exploradores/ascenso/ascenso';
 
 export const routes: Routes = [
@@ -10,7 +13,6 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
   
-  // Usamos renderMode: 'client' para saltar el error de prerendering
   { 
     path: ':grupo/ascenso', 
     component: AscensoComponent,
